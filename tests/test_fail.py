e97ade9a4262af46706dcb4f5bca8e547c954444
sjfuentes-uniandes <sj.fuentes@uniandes.edu.co>
2026-04-26 10:27:33 -0500

    retirar prueba fallida

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -1,5 +1,5 @@
-"""Test que falla intencionalmente para verificar el pipeline."""
+# """Test que falla intencionalmente para verificar el pipeline."""
 
 
-def test_should_fail():
-    assert 1 == 2, "Este test falla a propósito"
+# def test_should_fail():
+#     assert 1 == 2, "Este test falla a propósito"
